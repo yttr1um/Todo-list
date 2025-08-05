840cd1e364d3515cfa05868baeb71931b8329603
Salem Alhaddad <85304055+yttr1um@users.noreply.github.com>
2025-08-05 16:54:42 +0400

    Ability to add projects using prompt

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./styles.css";
 
 const sidebar = document.querySelector(".sidebar");
+const addPrjBtn = document.querySelector(".add-project-btn");
 
 function addProject(name) {
     const project = document.createElement("div");
@@ -8,5 +9,10 @@ function addProject(name) {
     project.classList.add('project');
     sidebar.appendChild(project);
 }
+
+addPrjBtn.addEventListener("click", () => {
+    let name = prompt("Add project name: ");
+    addProject(name);
+})
 
 addProject('default');
