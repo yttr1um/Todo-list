daa752ddf567d32a583142a774c13db44c463a22
Salem Alhaddad <85304055+yttr1um@users.noreply.github.com>
2025-08-05 18:08:31 +0400

    Ability to add notes (default text)

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import "./styles.css";
 const sidebar = document.querySelector(".sidebar");
 const addPrjBtn = document.querySelector(".add-project-btn");
 
+const notes = document.querySelector(".notes");
+const addNoteBtn = document.querySelector(".main-content > button");
+
 function addProject(name) {
     const project = document.createElement("div");
     project.textContent = name;
@@ -10,9 +13,21 @@ function addProject(name) {
     sidebar.appendChild(project);
 }
 
+function addNote(text) {
+    const note = document.createElement("div");
+    note.textContent = text;
+    note.classList.add("note");
+    notes.appendChild(note);
+}
+
+addNoteBtn.addEventListener("click", () => {
+    const text = "test";
+    addNote(text);
+});
+
 addPrjBtn.addEventListener("click", () => {
     let name = prompt("Add project name: ");
     addProject(name);
-})
+});
 
 addProject('default');
