3990e9443afddf691ae011baa23d1d37c272beac
Salem Alhaddad <85304055+yttr1um@users.noreply.github.com>
2025-08-05 16:50:18 +0400

    add project function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import "./styles.css";
+
+const sidebar = document.querySelector(".sidebar");
+
+function addProject(name) {
+    const project = document.createElement("div");
+    project.textContent = name;
+    project.classList.add('project');
+    sidebar.appendChild(project);
+}
+
+addProject('default');
