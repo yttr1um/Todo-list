82d9c36b1b6e1868584eef98dfe91641b2093805
Salem Alhaddad <85304055+yttr1um@users.noreply.github.com>
2025-08-06 09:33:38 +0400

    Write directly on the notes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import "./styles/styles.css";
+import "./styles/notes.css";
+
+import { addNoteBtn, createNote} from "./notes";
 
 const sidebar = document.querySelector(".sidebar");
 const addPrjBtn = document.querySelector(".add-project-btn");
-
-const notes = document.querySelector(".notes");
-const openModalBtn = document.querySelector(".main-content > button");
 
 function addProject(name) {
     const project = document.createElement("div");
@@ -13,45 +13,13 @@ function addProject(name) {
     sidebar.appendChild(project);
 }
 
-function createNote() {
-    const colors = ["#FB64B6", "#21BCFF", "#FFDF20", "#7CCF35"];
-    const randIndex = Math.floor(Math.random() * 4);
-
-    const note = document.createElement("div");
-    note.classList.add("note");
-    note.style.backgroundColor = colors[randIndex];
-
-    const noteTitle = document.createElement("h3");
-    noteTitle.textContent = title.value;
-
-    const noteText = document.createElement("p");
-    noteText.textContent = text.value;
-
-    note.appendChild(noteTitle);
-    note.appendChild(noteText);
-
-    notes.appendChild(note);
-}
-
-addNoteBtn.addEventListener("click", () => {
-    //TODO: let the user choose color in modal.
-    createNote();
-
-    //close the modal after inserting the note.
-    closeModal();
-})
-
-//make this more organised
-openModalBtn.addEventListener("click", () => {
-    openModal();
-});
-
-closeModalBtn.addEventListener("click", () => {
-    closeModal();
-})
-
 addPrjBtn.addEventListener("click", () => {
     let name = prompt("Add project name: ");
     addProject(name);
 });
+
+addNoteBtn.addEventListener("click", () => {
+    createNote();
+});
+
 addProject('default');
---
+++ src/notes.js
@@ -0,0 +1,22 @@
+const notes = document.querySelector(".notes");
+export const addNoteBtn = document.querySelector(".main-content > button");
+
+export function createNote() {
+    const colors = ["#FB64B6", "#21BCFF", "#FFDF20", "#7CCF35"];
+    const randIndex = Math.floor(Math.random() * 4);
+
+    const note = document.createElement("div");
+    note.classList.add("note");
+    note.style.backgroundColor = colors[randIndex];
+
+    const noteTitle = document.createElement("input");
+    noteTitle.placeholder = "Note title";
+
+    const noteText = document.createElement("textarea");
+    noteText.placeholder = "Enter text here...";
+
+    note.appendChild(noteTitle);
+    note.appendChild(noteText);
+
+    notes.appendChild(note);
+}
