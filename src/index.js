b1ed3787096638d7d4f58f2ae8251782699ebec2
Salem Alhaddad <85304055+yttr1um@users.noreply.github.com>
2025-08-07 19:56:17 +0400

    changed sidebar design to fit the theme

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import "./styles/styles.css";
 import "./styles/notes.css";
+import "./styles/sidebar.css";
 
 import { addNoteBtn, createNote} from "./notes";
 
-const sidebar = document.querySelector(".sidebar");
-const addPrjBtn = document.querySelector(".add-project-btn");
+//const sidebar = document.querySelector(".sidebar");
+//const addPrjBtn = document.querySelector(".add-project-btn");
 
-function addProject(name) {
-    const project = document.createElement("div");
-    project.textContent = name;
-    project.classList.add('project');
-    sidebar.appendChild(project);
-}
+// function addProject(name) {
+//     const project = document.createElement("div");
+//     project.textContent = name;
+//     project.classList.add('project');
+//     sidebar.appendChild(project);
+// }
 
-addPrjBtn.addEventListener("click", () => {
-    let name = prompt("Add project name: ");
-    addProject(name);
-});
+// addPrjBtn.addEventListener("click", () => {
+//     let name = prompt("Add project name: ");
+//     addProject(name);
+// });
 
 addNoteBtn.addEventListener("click", () => {
     createNote();
